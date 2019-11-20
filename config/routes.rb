be9ceaf27128e02260f7a192ff2cbe6631046667
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-11-20 14:31:10 +0800

    Integrated new review form into show of restaurant and beautified

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'restaurants/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
 end
