f4c40b241657f273e68a0e599b9997803a3bee9b
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-11-20 13:10:38 +0800

    Created first yelp mock-up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Restaurant.create(name: Faker::Hipster.word, address: "#{Faker::Address.street_address}, #{Faker::Address.city}", category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone_number: Faker::PhoneNumber.cell_phone)
+end
